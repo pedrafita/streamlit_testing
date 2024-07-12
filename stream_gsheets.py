477c510ee0ba3284dd90c7dc381f6f45615ca097
Manu Ledo <pedrafita@users.noreply.github.com>
2024-07-11 20:25:19 -0400

    Add files via upload

---
+++ stream_gsheets.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 21:58:18 2024
+
+@author: manu
+"""
+
+# streamlit_app.py
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+df
+
+
+
+df2 = st.data_editor(df)
+
+df2 = conn.update(worksheet="Conference",data=df2)
